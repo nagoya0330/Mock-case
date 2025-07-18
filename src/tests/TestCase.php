2c2e6b50589ed3a5a2739c4e1663553d356193df
nagoyamiho <mh30032000@gmail.com>
2025-07-18 15:05:50 +0900

    テスト修正

--- src/tests/TestCase.php
+++ src/tests/TestCase.php
@@ -7,4 +7,12 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // テスト中はCSRFミドルウェアを無効化（419回避）
+        $this->withoutMiddleware(\App\Http\Middleware\VerifyCsrfToken::class);
+    }
 }
