7bf9657e734f2f19f8c2d3d02e46a593847d4c90
nagoyamiho <mh30032000@gmail.com>
2025-08-01 01:32:45 +0900

    DatabaseSeeder.php を修正

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -3,18 +3,23 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run(): void
     {
-    $this->call([
-        ItemSeeder::class,
-    ]);
+        // ユーザーの作成
+        User::factory()->create(['id' => 1, 'name' => 'User1', 'email' => 'user1@example.com']);
+        User::factory()->create(['id' => 2, 'name' => 'User2', 'email' => 'user2@example.com']);
+        User::factory()->create(['id' => 3, 'name' => 'User3', 'email' => 'user3@example.com']);
+        User::factory()->create(['id' => 4, 'name' => 'User4', 'email' => 'user4@example.com']);
+        User::factory()->create(['id' => 5, 'name' => 'User5', 'email' => 'user5@example.com']);
+
+        // Seederの呼び出し
+        $this->call([
+            CategorySeeder::class, // ← これを追加
+            ItemSeeder::class,
+        ]);
     }
 }
