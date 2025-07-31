945dd9da149cec82af530940c51e9945817cf66d
nagoyamiho <mh30032000@gmail.com>
2025-08-01 01:58:58 +0900

    DatabaseSeeder.php を修正

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -9,16 +9,14 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        // ユーザーの作成
         User::factory()->create(['id' => 1, 'name' => 'User1', 'email' => 'user1@example.com']);
         User::factory()->create(['id' => 2, 'name' => 'User2', 'email' => 'user2@example.com']);
         User::factory()->create(['id' => 3, 'name' => 'User3', 'email' => 'user3@example.com']);
         User::factory()->create(['id' => 4, 'name' => 'User4', 'email' => 'user4@example.com']);
         User::factory()->create(['id' => 5, 'name' => 'User5', 'email' => 'user5@example.com']);
 
-        // Seederの呼び出し
         $this->call([
-            CategorySeeder::class, // ← これを追加
+            CategorySeeder::class,
             ItemSeeder::class,
         ]);
     }
